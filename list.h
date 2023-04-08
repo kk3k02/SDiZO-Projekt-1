625e441f3f877c6318c0d0f346d187cad5e03af9
Jakub Jakubowicz <kubajak@outlook.com>
2023-04-08 12:43:20 +0200

    Dodano konstr., destr. i prototypy funkcji do sterowania listą jednokierunkową

--- list.h
+++ list.h
@@ -1,14 +1,45 @@
-//
-// Created by kubaj on 08.04.2023.
-//
+/*
+ * Autor: Jakub Jakubowicz
+ * Data: Kwiecien 2023
+ * Plik: list.h
+ *
+ * Opis: Klasa definiujaca liste jednoelementowa
+ * oraz funkcje pozwalajace na dodawanie, usuwanie,
+ * wyswietlanie i wczytywanie elementow.
+ */
 
 #ifndef SDIZO_PROJEKT_NR_1_LIST_H
 #define SDIZO_PROJEKT_NR_1_LIST_H
 
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+
+using namespace std;
 
 class list {
 
-};
+    int value; // Zmienna typu int do przechowywania wartosci elementu listy
+    list *ptr; // Wskaznik na nastepny element listy
+public:
 
+    list(); //konstruktor - wywoływany automatycznie przy tworzeniu obieku
+    ~list(); //destrukor - wywływany automatycznie przy usuwaniu obiektu
+
+    static void loadFromFile(const string& FileName); // Wczytywanie danych z pliku
+
+    static int IsValueInList(int value); // Sprawdzanie czy podana wartosc znajduje sie w liscie
+
+    static void addElement(int val, int opt); // Dodawanie elementu o podanej wartosci
+
+    static void deleteElement(int opt); // Usuwanie elementu z listy
+
+    static void display(); // Wyswietlanie wszystkich elementow listy
+
+    static void generateList(int size); // Generowanie listy o podanym rozmiarze wypelnionej losowymi wartosciami
+
+    static int countList(); // Zwracanie liczby elementow w liscie
+
+};
 
 #endif //SDIZO_PROJEKT_NR_1_LIST_H
